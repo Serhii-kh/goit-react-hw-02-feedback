70e33e496261f05b28425b3de764711fc8a33021
Serhii Naboka <drifterzzz@gmail.com>
2023-04-26 14:41:43 +0300

    in proc

--- src/components/feedback/feedback.jsx
+++ src/components/feedback/feedback.jsx
@@ -13,31 +13,33 @@ export class Feedback extends React.Component {
       <div className={css.feedback__wrapper}>
         <div className={css.feedback}>
           <h1 className={css.feedback__title}>Please leave feedback</h1>
-          <button type="button">Good</button>
-          <button type="button">Neutral</button>
-          <button type="button">Bad</button>
+          <div className={css.buttons__wrapper}>
+            <button type="button">Good</button>
+            <button type="button">Neutral</button>
+            <button type="button">Bad</button>
+          </div>
         </div>
-        <div className={css.feedback__statistics}>
-          <h2 className={css.feedback}>Statistics</h2>
+        <div className={css.statistics}>
+          <h2 className={css.statistics__title}>Statistics</h2>
           <p>
             Good:
-            <span className={css.feedback__quantity}></span>
+            <span className={css.statistics__good}></span>
           </p>
           <p>
             Neutral:
-            <span className={css.feedback__quantity}></span>
+            <span className={css.statistics__neutral}></span>
           </p>
           <p>
             Bad:
-            <span className={css.feedback__quantity}></span>
+            <span className={css.statistics__bad}></span>
           </p>
           <p>
             Total:
-            <span className={css.feedback__quantity}></span>
+            <span className={css.statistics__total}></span>
           </p>
           <p>
             Positive feedback:
-            <span className={css.feedback__quantity}></span>
+            <span className={css.statistics__positive}></span>
           </p>
         </div>
       </div>
