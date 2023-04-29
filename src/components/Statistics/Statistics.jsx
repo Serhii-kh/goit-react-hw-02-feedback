507f79f1a083b14caf4727d30ee7573de990842f
Serhii Naboka <drifterzzz@gmail.com>
2023-04-29 18:05:38 +0300

    upd-6

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,9 +3,9 @@ import css from '../Statistics/Statistics.module.css';
 export const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
 	<div className={css.statistics__list}>
 		<p className={css.statistics__item}>Good: {good}</p>
-		<p className={css.statistics__item}>Neutral:{neutral}</p>
+		<p className={css.statistics__item}>Neutral: {neutral}</p>
 		<p className={css.statistics__item}>Bad: {bad}</p>
 		<p className={css.statistics__item}>total: {total}</p>
-		<p className={css.statistics__item}>{positivePercentage} %</p>
+		<p className={css.statistics__item}>Positive feedback: {positivePercentage} %</p>
 	</div>
 );
